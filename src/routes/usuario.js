a7d5a2df9d4c2247c3566bc297e29a2f8acdd364
DavidWelchez <davidwelchez@gmail.com>
2021-02-16 20:50:55 -0600

    vista y metodo para el perfil de usuario

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -159,4 +159,105 @@ router.post('/edit/:id', async (req, res) => {
     res.redirect('/usuario');
 });
 
+
+
+router.get('/perfil', isLoggedIn, async (req, res) => {
+    var loginAdmin = false;
+    var loginGeneral = false;
+
+    rol = req.user.rol;
+    if(rol == "Admin") {
+        loginAdmin = true;
+        }
+        if(rol == "General") {
+            loginGeneral = true;
+            }
+    const perfilusuario = req.user;
+    res.render('usuario/perfil', { 
+        
+        layout: "dashboard",
+        loginAdmin,
+        loginGeneral,
+        perfilusuario 
+    });
+});
+
+
+
+router.post('/editperfil/:id', async (req, res) => {
+    const { id } = req.params;
+    const { fullname } = req.body;
+    const newusuario = {
+        fullname,
+        
+
+        
+    };
+    await pool.query('UPDATE users set ? WHERE id = ?', [newusuario, id]);
+    req.flash('success', 'Usuario actualizado');
+    res.redirect('/usuario/perfil');
+});
+
+
+
+
+router.post('/editpassperfil/:id', async (req, res) => {
+    const { id } = req.params;
+    const { password,newpassword, confirmpassword} = req.body;
+
+    const rows = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
+    if (rows.length > 0) {
+      const user = rows[0];
+      const validPassword = await helpers.matchPassword(password, user.password)
+      if (validPassword ) {
+          if (newpassword == confirmpassword) {
+            const newpass= {
+                password
+                
+            };
+            newpass.password = await helpers.encryptPassword(newpassword);
+            await pool.query('UPDATE users set ? WHERE id = ?', [newpass, id]);
+            req.flash('success', 'Contraseña actualizado');
+            res.redirect('/usuario/perfil'); 
+          }else{
+             req.flash('message', 'Contraseñas no coinciden');
+             res.redirect('/usuario/perfil#contra'); 
+          }
+        
+      } else {
+         req.flash('message', 'Contraseña actual es incorrecta');
+         res.redirect('/usuario/perfil#contra');       }
+    } 
+
+
+    
+});
+
+router.post('/buscar', isLoggedIn,roles, async (req, res) => {
+    var loginAdmin = false;
+    var loginGeneral = false;
+
+    rol = req.user.rol;
+    if(rol == "Admin") {
+        loginAdmin = true;
+        }
+        if(rol == "General") {
+            loginGeneral = true;
+            }
+
+     const { fullname } = req.body;
+    const usuario = await pool.query('SELECT * FROM users WHERE fullname = ?', [fullname]);
+    console.log(fullname)
+    res.render('usuario/buscar', { 
+        
+        layout: "dashboard",
+        loginAdmin,
+        loginGeneral,
+        usuario 
+    });
+});
+
+
+
+
 module.exports = router;
