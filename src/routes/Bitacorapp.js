aec1bcb4b6af880bbc68eb763f7ca7ab6fe50191
DavidWelchez <davidwelchez@gmail.com>
2021-03-26 20:30:33 -0600

    bitacorapp

--- src/routes/Bitacorapp.js
+++ src/routes/Bitacorapp.js
@@ -12,7 +12,7 @@ router.use(express.json());
 router.get('/list' , async (req, res) => {
   const formatYmd= date=> date.toISOString().slice(0,10);
     
-  const bitacora = await pool.query(' SELECT bitacoras.id, bitacoras.fechaDeIncidencia,bitacoras.horaDeIncidencia,incidentes.incidente,plataformas.plataforma,eventos.evento,bitacoras.descripcion,u.fullname,ua.fullname as atendio,proveedors.proveedor,bitacoras.fechaSolucion, bitacoras.horaSolucion, bitacoras.estado,eventoRiesgos.EventoRiesgo,factorRiesgos.factor,bitacoras.archivo FROM bitacoras, users as u,users as ua, proveedors,plataformas, incidentes,factorRiesgos,eventos,eventoRiesgos where plataformas.incidenteId=incidentes.id and bitacoras.plataformaId=plataformas.id and bitacoras.eventoId=eventos.id and bitacoras.userId=u.id  and bitacoras.atendioid=ua.id and bitacoras.proveedorId=proveedors.id and factorRiesgos.eventoRiesgoId=eventoRiesgos.id and bitacoras.factorRiesgoId=factorRiesgos.id ');
+  const bitacora = await pool.query(' SELECT bitacoras.id, bitacoras.fechaDeIncidencia,bitacoras.horaDeIncidencia,incidentes.incidente,plataformas.plataforma,eventos.evento,bitacoras.descripcion,u.fullname,ua.fullname as atendio,proveedors.proveedor,bitacoras.fechaSolucion, bitacoras.horaSolucion, bitacoras.estado,eventoRiesgos.EventoRiesgo,factorRiesgos.factor,bitacoras.archivo FROM bitacoras, users as u,users as ua, proveedors,plataformas, incidentes,factorRiesgos,eventos,eventoRiesgos where plataformas.incidenteId=incidentes.id and bitacoras.plataformaId=plataformas.id and bitacoras.eventoId=eventos.id and bitacoras.userId=u.id  and bitacoras.atendioid=ua.id and bitacoras.proveedorId=proveedors.id and factorRiesgos.eventoRiesgoId=eventoRiesgos.id and bitacoras.factorRiesgoId=factorRiesgos.id  order by id desc');
       
 bitacora.forEach(element => {
           
@@ -61,8 +61,46 @@ router.post('/addBitacora' , (req, res) => {
     });
 });
 
+/** */
+router.get('/edit/:bitacoraid', async (req, res) => {
+  const formatYmd= date=> date.toISOString().slice(0,10);
+  const { bitacoraid } = req.params;
+  const bitacora = await pool.query('SELECT * FROM bitacoras WHERE id = ?', [bitacoraid]);      
+bitacora.forEach(element => {
+          
+          element.fechaDeIncidencia= formatYmd(element.fechaDeIncidencia)
+          element.fechaSolucion= formatYmd(element.fechaSolucion)
+      });
 
+  
+      res.json(bitacora)
+  
+});
 
+router.get('/edit2/:bitacoraid', async (req, res) => {
+  const formatYmd= date=> date.toISOString().slice(0,10);
+  const { bitacoraid } = req.params;
+  const bitacora = await pool.query(' SELECT bitacoras.id, bitacoras.fechaDeIncidencia,bitacoras.horaDeIncidencia,incidentes.incidente,plataformas.plataforma,eventos.evento,bitacoras.descripcion,u.fullname,ua.fullname as atendio,proveedors.proveedor,bitacoras.fechaSolucion, bitacoras.horaSolucion, bitacoras.estado,eventoRiesgos.EventoRiesgo,factorRiesgos.factor,bitacoras.archivo FROM bitacoras, users as u,users as ua, proveedors,plataformas, incidentes,factorRiesgos,eventos,eventoRiesgos WHERE plataformas.incidenteId=incidentes.id and bitacoras.plataformaId=plataformas.id and bitacoras.eventoId=eventos.id and bitacoras.userId=u.id  and bitacoras.atendioid=ua.id and bitacoras.proveedorId=proveedors.id and factorRiesgos.eventoRiesgoId=eventoRiesgos.id and bitacoras.factorRiesgoId=factorRiesgos.id  and bitacoras.id = ?', [bitacoraid]);
+bitacora.forEach(element => {
+          
+          element.fechaDeIncidencia= formatYmd(element.fechaDeIncidencia)
+          element.fechaSolucion= formatYmd(element.fechaSolucion)
+      });
+
+  
+      res.json(bitacora)
+  
+});
+
+router.post('/edit/:bitacoraid' ,(req, res) => {
+  const obj = req.body
+  const { bitacoraid } = req.params;
+ const query =('UPDATE bitacoras set ? WHERE id = ?');
+  //const query = "INSERT INTO bitacoras SET ?";  
+  pool.query (query, [obj, bitacoraid], function(error, results, fields){
+     res.json(results.insertId)
+  });
+});
 
 
 module.exports = router;
